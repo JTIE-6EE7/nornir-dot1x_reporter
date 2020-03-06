70d8802a7ce75edac5761ae7e6195cab20bc5ac4
JT <foosyou@gmail.com>
2020-03-06 13:19:14 -0600

    wafflez

--- dot1x_report.py
+++ dot1x_report.py
@@ -8,19 +8,23 @@ from nornir import InitNornir
 from nornir.core.filter import F
 from nornir.plugins.functions.text import print_result
 from nornir.plugins.tasks.networking import netmiko_send_command
+import matplotlib.pyplot as plt
+from pywaffle import Waffle
 
 
 # Run show commands on each switch
 def run_commands(task):
     print(f'{task.host}: running show comands.')
     # run "show version" on each host
-    sh_version = task.run(
+    sh_dot1x = task.run(
         task=netmiko_send_command,
         command_string="show dot1x all",
         use_textfsm=True,
     )
 
-    print(run_commands.result)
+    print(sh_dot1x.result)
+
+
 
 
 def main():
@@ -29,9 +33,21 @@ def main():
     nr = InitNornir()
     # filter The Norn
     nr = nr.filter(platform="cisco_ios")
-        # run The Norn run commands
+    # run The Norn run commands
     nr.run(task=run_commands)
     
+
+
+wafflez = plt.figure(
+    FigureClass=Waffle,
+    rows=5,
+    columns=10,
+    values={'switch stacks with\ndot1x enabled': 20, 'switch stacks with\ndot1x disabled': 12},
+    legend={'loc': 'upper left', 'bbox_to_anchor': (1, 1)},
+    colors=["#008000", "#F51B00"]
+)
+
+plt.show(wafflez)
 
 if __name__ == "__main__":
     main()
